1211d76bc5baa00320299d5653d9427cb8d9f954
Gerryl Esperacion <Gerrylesperacion@gmail.com>
2021-07-21 21:37:25 -0700

    merge

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,35 +10,3 @@ router.use('/photos', photoRouter);
 
 module.exports = router;
 
-// const asyncHandler = require('express-async-handler');
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { restoreUser } = require('../../utils/auth.js');
-// const { requireAuth } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-
-// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'Demo-lition'
-//     },
-//   })
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// }));
-
-// router.get(
-//   '/restore-user',
-//   restoreUser,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// //test require auth middleware //suppose to get Unauthorized
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
