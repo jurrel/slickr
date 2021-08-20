d00d635c2df436a96c273f6803efb1f2bb86561a
Gerryl Esperacion <Gerrylesperacion@gmail.com>
2021-08-20 08:39:36 -0700

    fixed landing page css

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -4,7 +4,7 @@ const asyncHandler = require('express-async-handler');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const { User, Photo } = require('../../db/models');
 
 
 const validateSignup = [
@@ -50,5 +50,16 @@ router.post(
     });
   }),
 );
+
+//gets post related to the user
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+  const { id } = req.params
+  const findUserPhotos = await Photo.findAll({
+    where: { userId: id },
+    include: User
+  })
+  return res.json(findUserPhotos)
+}));
+
 
 module.exports = router;
