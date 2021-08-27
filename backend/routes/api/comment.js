247c9a80d9afa8f9cca0aa73046d8d523c5c5caa
Gerryl Esperacion <Gerrylesperacion@gmail.com>
2021-08-27 15:30:17 -0700

    delete button

--- backend/routes/api/comment.js
+++ backend/routes/api/comment.js
@@ -27,12 +27,22 @@ router.post('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
 })); 
 
 //Does not delete 
-// router.delete('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+router.delete('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const comment = await Comment.findByPk(id)
+    await comment.destroy();
+    res.status(204).end()
+}))
+
+// router.put('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
 //     const { id } = req.params
 //     const comment = await Comment.findByPk(id)
-//     await comment.destroy();
-//     res.status(204).end()
+//     const { comment } = req.body
+//     await comment.update({ comment })
+//     return res.json(comment)
 // }))
+
+
 
 
 module.exports = router; 
